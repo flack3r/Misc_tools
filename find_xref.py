d17a3681189d4526fc21ef604bf3b18f6ae60aba
kyj9206 <kyj9206@gmail.com>
2017-01-22 16:56:36 +0900

    first commit

---
+++ find_xref.py
@@ -0,0 +1,70 @@
+from idaapi import *
+import idc 
+
+mark_list = []
+def check_refs(fun):
+	addr = LocByName( fun )
+	refs_list = []
+	if addr != BADADDR:
+			cross_refs = CodeRefsTo( addr, 0)
+			print "[*]refs to %s" %(fun)
+			print "======================"
+			for ref in cross_refs:
+					print "%08x" %(ref)
+					refs_list.append(ref)
+	return refs_list
+
+
+def Mark(addr, find):
+	global mark_list
+	f = get_func(addr)
+	if not f:
+		print "[*]0x%x is not function" %(addr)
+	fc = FlowChart(f)
+	for block in fc:
+		addr = block.startEA
+		while True:
+			arg_no = GetDisasm(addr).count(",")
+			for i in range(arg_no):
+				if GetOpnd(addr, i) == find:
+					idc.SetColor(addr, CIC_ITEM, 0xffff00)
+					mark_list.append(addr)
+
+			if idc.NextHead(addr) > block.endEA:
+					break
+			addr = idc.NextHead(addr)
+
+def MarkClear():
+	global mark_list
+	for addr in mark_list:
+		idc.SetColor(addr, CIC_ITEM, 0xffffff)
+	mark_list = []
+
+def Check_get( fun ):
+	refs_list = check_refs(fun)
+	for refs in refs_list:
+		flag = 0
+		f = get_func(refs)
+		if not f:
+			continue
+		fc = FlowChart(f)
+		for block in fc:
+			addr = block.startEA
+			while True:
+				arg1 = GetOpnd(addr, 0)
+				if arg1 == 'req_get_cstream_var':
+					flag = 1
+					break
+				if idc.NextHead(addr) > block.endEA:
+					break
+				addr = idc.NextHead(addr)
+			if flag == 1:
+				break
+		if flag == 1:
+			print "[*]possible exploit refs 0x%x" %(refs)
+
+def man():
+	print "[1] def check_refs(fun_name) => check func reference"
+	print "[2] def Check_get(fun_name) => check req_get_cstream_var"
+	print "[3] def Mark(addr, find_string) => marking argument"
+	print "[4] def MarkClear() => Marking Clear"
